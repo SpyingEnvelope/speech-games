e24649c53b5b11ae114adb2a77bec48715772528
SpyingEnvy <gadi800@gmail.com>
2022-10-24 20:45:28 -0600

    added a slider to change games. made it possible to select games

--- gamestate.py
+++ gamestate.py
@@ -1,4 +1,4 @@
-game_state = 'flash_init'
+game_state = 'main_menu'
 
 # function to set the global game state for the game
 def set_game_state(name):
--- main_menu.py
+++ main_menu.py
@@ -35,6 +35,7 @@ cariboo_btn_rect = cariboo_btn.get_rect(center = (screen.get_width() * 0.5, scre
 
 cariboo_background_1 = pygame.image.load('graphics/backgrounds/platformer_background_4/background_cariboo.png').convert()
 cariboo_background_1 = pygame.transform.scale(cariboo_background_1, (screen.get_width(), screen.get_height()))
+cariboo_background_rect = cariboo_background_1.get_rect(topleft = (0, 0))
 
 close_button = pygame.image.load('graphics/buttons/close_btn.png')
 close_button = pygame.transform.scale(close_button, (screen.get_width() * 0.03, screen.get_height() * 0.06))
@@ -50,6 +51,10 @@ cariboo_text = pixel_font_med.render('CARIBOO', False, (255,255,255))
 cariboo_text_rect = cariboo_text.get_rect(center = (screen.get_width() * 0.5, screen.get_height() * 0.12))
 cariboo_text_black = pixel_font_med.render('CARIBOO', False, (0,0,0))
 
+flash_text = pixel_font_med.render('FLASH CARDS', False, (255,255,255))
+flash_text_rect = flash_text.get_rect(center = (screen.get_width() * 0.5 + screen.get_width(), screen.get_height() * 0.12))
+flash_text_black = pixel_font_med.render('FLASH CARDS', False, (0,0,0))
+
 btn_border_black = pygame.Surface((cariboo_btn.get_width() * 1.01, cariboo_btn.get_height() * 1.01))
 btn_border_white = pygame.Surface((cariboo_btn.get_width() * 1.01, cariboo_btn.get_height() * 1.01))
 btn_border_white.fill((255,255,255))
@@ -57,6 +62,11 @@ btn_border_black.fill((0,0,0))
 
 flash_background = pygame.Surface((screen.get_width(), screen.get_height()))
 flash_background.fill((94,129,162))
+flash_background_rect = flash_background.get_rect(topleft = (screen.get_width(), 0))
+
+flash_btn = pygame.image.load('graphics/main_menu/flash_cards.png').convert()
+flash_btn = pygame.transform.scale(flash_btn, (screen.get_width() * 0.65, screen.get_height() * 0.65))
+flash_btn_rect = flash_btn.get_rect(center = (screen.get_width() * 0.5 + screen.get_width(), screen.get_height() * 0.5))
 
 left_arrow_w = pygame.image.load('graphics/main_menu/left_arrow_white.png').convert_alpha()
 left_arrow_g = pygame.image.load('graphics/main_menu/left_arrow_gray.png')
@@ -71,10 +81,41 @@ right_arrow_w = pygame.transform.scale(right_arrow_w, (screen.get_width() * 0.04
 right_arrow_rect = right_arrow_w.get_rect(center = (screen.get_width() * 0.9, screen.get_height() * 0.5))
 left_arrow_rect = left_arrow_g.get_rect(center = (screen.get_width() * 0.1, screen.get_height() * 0.5))
 
+game_btn_left = cariboo_btn_rect.left
+game_btn_top = cariboo_btn_rect.top
+
+def move_menu(direction):
+    global cariboo_background_rect
+    global flash_background_rect
+    global cariboo_btn_rect
+    global cariboo_text_rect
+    global flash_text_rect
+    global flash_btn_rect
+
+    if direction == 'right' and cariboo_background_rect.left == 0:
+        cariboo_background_rect.left -= screen.get_width()
+        cariboo_btn_rect.left -= screen.get_width()
+        cariboo_text_rect.left -= screen.get_width()
+
+        flash_background_rect.left -= screen.get_width()
+        flash_text_rect.left -= screen.get_width()
+        flash_btn_rect.left -= screen.get_width()
+    
+    if direction == 'left' and cariboo_background_rect.left != 0:
+        cariboo_background_rect.left += screen.get_width()
+        cariboo_btn_rect.left += screen.get_width()
+        cariboo_text_rect.left += screen.get_width()
+
+        flash_background_rect.left += screen.get_width()
+        flash_text_rect.left += screen.get_width()
+        flash_btn_rect.left += screen.get_width()
+
 def menu():
     global game_state
     global select_game_text_animation
     global select_game_text_state
+    global flash_background_rect
+    global cariboo_background_rect
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -88,26 +129,37 @@ def menu():
 
             if cariboo_btn_rect.collidepoint(event.pos):
                 set_game_state('cariboo_init')
+            
+            if flash_btn_rect.collidepoint(event.pos):
+                set_game_state('flash_init')
+            
+            if right_arrow_rect.collidepoint(event.pos):
+                move_menu('right')
+
+            if left_arrow_rect.collidepoint(event.pos):
+                move_menu('left')
     
-    screen.blit(cariboo_background_1, (0,0))
+    screen.blit(cariboo_background_1, cariboo_background_rect)
+    screen.blit(flash_background, flash_background_rect)
     
-    screen.blit(btn_border_white, (cariboo_btn_rect.left - (screen.get_width() * 0.003), cariboo_btn_rect.top - (screen.get_height() * 0.003)))
-    screen.blit(btn_border_black, (cariboo_btn_rect.left - (screen.get_width() * 0.003), cariboo_btn_rect.top - (screen.get_height() * 0.003)))
+    screen.blit(btn_border_white, (game_btn_left - (screen.get_width() * 0.003), game_btn_top - (screen.get_height() * 0.003)))
+    screen.blit(btn_border_black, (game_btn_left - (screen.get_width() * 0.003), game_btn_top - (screen.get_height() * 0.003)))
     screen.blit(cariboo_btn, cariboo_btn_rect)
     screen.blit(cariboo_text, cariboo_text_rect)
     screen.blit(cariboo_text_black, cariboo_text_rect)
     cariboo_text_black.set_alpha(select_game_text_animation)
 
-    screen.blit(flash_background, (screen.get_width(), 0))
+    screen.blit(flash_text, flash_text_rect)
+    screen.blit(flash_text_black, flash_text_rect)
+    flash_text_black.set_alpha(select_game_text_animation)
+    screen.blit(flash_btn, flash_btn_rect)
 
     screen.blit(select_game, select_game_rect)
     screen.blit(select_game_black, select_game_rect)
     screen.blit(right_arrow_w, right_arrow_rect)
     screen.blit(left_arrow_w, left_arrow_rect)
     screen.blit(right_arrow_g, right_arrow_rect)
-    screen.blit(left_arrow_g, left_arrow_rect)
-    
-
+    screen.blit(left_arrow_g, left_arrow_rect)  
 
     btn_border_black.set_alpha(select_game_text_animation)
     right_arrow_g.set_alpha(select_game_text_animation)

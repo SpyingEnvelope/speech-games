6855587956f7c328423b590420b2c7ef9c45c095
SpyingEnvy <gadi800@gmail.com>
2022-11-05 15:02:04 -0600

    added fullscren for 1440p

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ screen_height = root.winfo_screenheight()
 
 # set display size
 if screen_height == 1440:
-    screen = pygame.display.set_mode((2560, 1440))
+    screen = pygame.display.set_mode((2560, 1440), pygame.FULLSCREEN)
 if screen_height == 2160:
     screen = pygame.display.set_mode((3840, 2160), pygame.FULLSCREEN)
 if screen_height == 1080:
